b4083d3c6fcc8ec01ab0f4564fa8f039319ef4e8
Vitaliy Kovalev <vitaliy.kovalev@playtech.com>
2014-08-21 18:01:41 +0300

    add option for configuration requests unread news

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 var alarmName = "ukrainian-news";
+var periodMin = 10;
+if (localStorage.getItem('background_period_config')) {
+    periodMin = parseInt(localStorage.getItem('background_period_config'));
+
+}
 
 chrome.alarms.create(alarmName, {
     delayInMinutes: 0,
-    periodInMinutes: 10
+    periodInMinutes: periodMin
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name === alarmName) {
+        alert('alarm!');
         chrome.browserAction.setBadgeText ( { text: newsGenerator.requestNews().length.toString() } );
     }
 });
