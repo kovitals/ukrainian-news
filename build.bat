24f8ba36bc899cb76952661051bf377da765a26a
Vitaliy Kovalev <vitaliy.kovalev@playtech.com>
2015-03-03 15:50:27 +0200

    some changes in build script

--- build.bat
+++ build.bat
@@ -7,5 +7,6 @@ copy *.css ukrainian-news
 copy *.html ukrainian-news
 copy img\*.* ukrainian-news\img
 copy resource\*.otf ukrainian-news\resource
+IF EXIST ukrainian-news.zip del /F ukrainian-news.zip
 zip ukrainian-news.zip ukrainian-news -r
 rmdir ukrainian-news /s/q
