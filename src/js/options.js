9979185b69c00f825289fbbd56abe22a15512a3e
valeriy-bashtovoy <bashtovoy.valeriy@gmail.com>
2017-04-21 00:01:52 +0300

    removed annoying alert;

--- src/js/options.js
+++ src/js/options.js
@@ -2,14 +2,18 @@ import common from './common';
 
 // Saves selected rss chanel to local storage
 function save_options() {
+
     var rss_channels_config = {};
     var rss_channels = document.getElementsByTagName('input');
+
     for (var i = 0; i < rss_channels.length; i++) {
         if (rss_channels[i].checked == true) {
             rss_channels_config[rss_channels[i].getAttribute('name').toString()] = rss_channels[i].checked.toString();
         }
     }
+
     common.options.setRSSChannels(JSON.stringify(rss_channels_config));
+
     if (document.getElementById(common.storageKey.updatePeriod).value) {
         common.options.setUpdatePeriod(document.getElementById(common.storageKey.updatePeriod).value);
         chrome.alarms.clear('ukrainian-news');
@@ -18,9 +22,9 @@ function save_options() {
             periodInMinutes: parseInt(common.options.getUpdatePeriod())
         });
     }
+
     common.options.setWindowWidth(document.getElementById(common.storageKey.windowWidth).value);
     common.options.setShowLastItems(document.getElementById(common.storageKey.showLastItems).value);
-    alert("Налаштування збережені");
 }
 
 // Restores selected rss channels from local storage
